a90a7455be0ddd7066eb511d9ab76497dc443bed
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2025-09-07 12:03:17 -0300

    Built web
    Branch: main
    Commit: 344989e59fa724fef403de3bffd73f96cce743f9 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4225914052"
+    serviceWorkerVersion: "3206758384"
   }
 });
