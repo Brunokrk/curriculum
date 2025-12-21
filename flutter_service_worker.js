563c4fc853dc154c67f305c41a424beb2030e1f9
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2025-12-21 12:10:52 -0300

    Built web
    Branch: main
    Commit: 125af02a243a38bf848ee34d6af8d546b7359ebc
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -65,7 +65,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "63613074bd6f0d6671224c1e17ba51ae
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "1e486158084c63aa18f1629e14de2713",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "185ecb45cea25eb2f3e374941d148b63",
+"flutter_bootstrap.js": "f6df2f707b56b9a348361f18df055e78",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
